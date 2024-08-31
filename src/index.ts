19304249b5a5d38ad0e70a74243ff05fe928bc0e
Projeto Inclus <projetoinclus@gmail.com>
2024-08-31 14:20:34 -0300

    2024

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("api");
+import { dataSourceMYSQL } from "./connection/data_source";
+
+const database = process.env.DB_DEFAULT_NAME || "";
+const port = process.env.SERVER_PORT || 3000;
+
+export const dataSource = dataSourceMYSQL(database, []);
+
+try {
+  dataSource.initialize();
+  console.log("CONECTADO");
+} catch (error) {
+  console.error("ERRO AO CONECTAR:", error);
+}
