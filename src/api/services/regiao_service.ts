a36f3e343d5824f7e324ab3d703c5cf0ede4d85b
Projeto Inclus <projetoinclus@gmail.com>
2024-09-14 15:00:12 -0300

    20240914-1459 - atualização serviço (getId, create, delete)

--- src/api/services/regiao_service.ts
+++ src/api/services/regiao_service.ts
@@ -2,7 +2,7 @@ import { dataSource } from "../..";
 import { Regiao } from "../entities/Regiao";
 import { Request, Response } from "express";
 
-export async function findAluno(req: Request, res: Response) {
+export async function getAllRegiao(req: Request, res: Response) {
   const repository = dataSource.getRepository(Regiao);
   const objects = await repository.find();
 
@@ -11,6 +11,67 @@ export async function findAluno(req: Request, res: Response) {
       message: "OK",
       error: null,
       result: objects,
+    });
+  } catch (error) {
+    res.json({
+      message: "ERRO",
+      error,
+      result: null,
+    });
+  }
+}
+
+export async function getIdRegiao(req: Request, res: Response) {
+  const repository = dataSource.getRepository(Regiao);
+  const id: number = Number(req.params.id);
+  const object = await repository.findOneBy({ id });
+
+  try {
+    res.json({
+      message: "OK",
+      error: null,
+      result: object,
+    });
+  } catch (error) {
+    res.json({
+      message: "ERRO",
+      error,
+      result: null,
+    });
+  }
+}
+
+export async function createRegiao(req: Request, res: Response) {
+  const repository = dataSource.getRepository(Regiao);
+  const body = req.body;
+  const objectCreate = repository.create(body);
+  const object = await repository.save(objectCreate);
+
+  try {
+    res.json({
+      message: "OK",
+      error: null,
+      result: object,
+    });
+  } catch (error) {
+    res.json({
+      message: "ERRO",
+      error,
+      result: null,
+    });
+  }
+}
+
+export async function deleteRegiao(req: Request, res: Response) {
+  const repository = dataSource.getRepository(Regiao);
+  const id: number = Number(req.params.id);
+  const result = await repository.delete({ id });
+
+  try {
+    res.json({
+      message: "OK",
+      error: null,
+      result: result.affected,
     });
   } catch (error) {
     res.json({
